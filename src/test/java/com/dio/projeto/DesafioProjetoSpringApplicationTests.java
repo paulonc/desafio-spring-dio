58fbff46b7a93966a78e0ab554fdd65ffaf15048
Paulo Victor <paulo.cavalcanti@ccc.ufcg.edu.br>
2022-12-03 21:29:32 -0300

    created project

---
+++ src/test/java/com/dio/projeto/DesafioProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
