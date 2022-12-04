780ce214ba099612a83897e9f29af1bfd9138b9e
Paulo Victor <paulo.cavalcanti@ccc.ufcg.edu.br>
2022-12-03 21:43:54 -0300

    created services

---
+++ src/main/java/com/dio/projeto/service/ClienteService.java
@@ -0,0 +1,17 @@
+package com.dio.projeto.service;
+
+import com.dio.projeto.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+    
+}
