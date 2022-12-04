6acd47186cf51a03df7fcd6438fdc04b10164b9a
Paulo Victor <paulo.cavalcanti@ccc.ufcg.edu.br>
2022-12-03 22:02:27 -0300

    implementing functionalities in the service

--- src/main/java/com/dio/projeto/service/ViaCepService.java
+++ src/main/java/com/dio/projeto/service/ViaCepService.java
@@ -1,12 +1,14 @@
 package com.dio.projeto.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.dio.projeto.model.Endereco;
 
 @FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+@Service
 public interface ViaCepService {
     
     @GetMapping("/{cep}/json/")
